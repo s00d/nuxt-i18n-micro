14037731a3164a1a4a2380b6b6f7112522037cb0
Pavel Kuzmin <Virus191288@gmail.com>
2024-08-19 23:32:24 +0500

    update: defineI18nRoute with locale

--- src/runtime/plugins/03.define.ts
+++ src/runtime/plugins/03.define.ts
@@ -1,10 +1,16 @@
 import type { ModuleOptions } from '../../module'
-import { defineNuxtPlugin, useRuntimeConfig } from '#app'
+import { defineNuxtPlugin, useNuxtApp, useRuntimeConfig } from '#app'
 import { useRoute, useRouter } from '#imports'
 
 interface State extends ModuleOptions {
   rootDir: string
 }
+
+// Рекурсивный тип для переводов
+type TranslationObject = string | { [key: string]: TranslationObject }
+
+// Тип для локалей
+type LocalesObject = Record<string, TranslationObject>
 
 export default defineNuxtPlugin((_nuxtApp) => {
   const config = useRuntimeConfig()
@@ -13,14 +19,35 @@ export default defineNuxtPlugin((_nuxtApp) => {
 
   const i18nConfig: State = config.public.i18nConfig as State
 
+  // Функция нормализации, которая объединяет массивы и объекты в единый массив строк
+  const normalizeLocales = (locales?: string[] | LocalesObject): LocalesObject => {
+    if (Array.isArray(locales)) {
+      // Если передан массив, преобразуем его в объект с пустыми значениями
+      return locales.reduce((acc, locale) => {
+        acc[locale] = {}
+        return acc
+      }, {} as LocalesObject)
+    }
+    else if (typeof locales === 'object' && locales !== null) {
+      // Если передан объект, возвращаем его как есть
+      return locales
+    }
+    return {}
+  }
+
   // Функция для определения i18n маршрута
-  const defineI18nRoute = (routeDefinition: { locales?: string[] }) => {
+  const defineI18nRoute = (routeDefinition: { locales?: string[] | Record<string, Record<string, string>> }) => {
     const currentLocale = (route.params.locale || i18nConfig.defaultLocale!).toString()
-    const { locales } = routeDefinition
+    const normalizedLocales = normalizeLocales(routeDefinition.locales)
     const { name } = route
 
-    // Проверяем, если текущая локаль не входит в допустимые локали
-    if (locales && !locales.includes(currentLocale)) {
+    // Если текущая локаль есть в объекте locales
+    if (normalizedLocales[currentLocale]) {
+      const translation = normalizedLocales[currentLocale]
+      const nuxtApp = useNuxtApp()
+      nuxtApp.$mergeTranslations(translation)
+    }
+    else {
       // Если локаль не допустима, перенаправляем на дефолтную локаль
       let defaultRouteName = name?.toString().replace('localized-', '')
       const resolvedRoute = router.resolve({ name: defaultRouteName })

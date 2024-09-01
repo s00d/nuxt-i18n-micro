1542de50144243b148abcc49aebd007aade527de
Pavel Kuzmin <Virus191288@gmail.com>
2024-09-01 16:07:03 +0500

    test: add tests for locales path

--- test/locale-slug-undefault.test.ts
+++ test/locale-slug-undefault.test.ts
@@ -67,3 +67,65 @@ test('Test Locale Path', async ({ page, goto }) => {
   await page.click('#activity-locale-skiing')
   await expect(page).toHaveURL('/en/change-activity/book-activity/skiing')
 })
+
+
+test('Test Locale Path de', async ({ page, goto }) => {
+  // Test main activity page
+  await goto('/activity', { waitUntil: 'hydration' })
+  await goto('/de/activity', { waitUntil: 'hydration' })
+
+  // Check the URL and text content
+  await expect(page).toHaveURL('/de/activity')
+  await expect(page.locator('h1')).toHaveText('Activity Parent Page')
+
+  // Check links on the page
+  await expect(page.locator('#activity')).toHaveAttribute('href', '/de/activity')
+  await expect(page.locator('#activity-skiing')).toHaveAttribute('href', '/de/activity/skiing')
+  await expect(page.locator('#activity-hiking')).toHaveAttribute('href', '/de/activity/hiking')
+  await expect(page.locator('#activity-skiing-locale')).toHaveAttribute('href', '/de/activity/aktivitaet-buchen/ski-fahren')
+  await expect(page.locator('#activity-hiking-locale')).toHaveAttribute('href', '/de/activity/aktivitaet-buchen/wandern')
+  await expect(page.locator('#activity-locale')).toHaveAttribute('href', '/de/change-buchen')
+
+  // Test link navigation
+  await page.click('#activity-skiing')
+  await expect(page).toHaveURL('/de/activity/skiing')
+  await expect(page.locator('#title')).toHaveText('Skiing Subpage')
+  await expect(page.locator('#text')).toHaveText('Willkommen auf der Seite!')
+
+  // Test localized activity page
+  await goto('/de/activity', { waitUntil: 'hydration' })
+
+  await page.click('#activity-hiking')
+  await expect(page).toHaveURL('/de/activity/hiking')
+  await expect(page.locator('#title')).toHaveText('Hiking Subpage')
+  await expect(page.locator('#text')).toHaveText('Willkommen auf der Seite!')
+
+  await goto('/de/activity', { waitUntil: 'hydration' })
+
+  await page.click('#activity-skiing-locale')
+  await expect(page).toHaveURL('/de/activity/aktivitaet-buchen/ski-fahren')
+  await expect(page.locator('#title')).toHaveText('Skiing Locale Subpage')
+  await expect(page.locator('#text')).toHaveText('Willkommen auf der Seite!')
+
+  await goto('/de/activity', { waitUntil: 'hydration' })
+
+  await page.click('#activity-hiking-locale')
+  await expect(page).toHaveURL('/de/activity/aktivitaet-buchen/wandern')
+  await expect(page.locator('#title')).toHaveText('Hiking Locale Subpage')
+  await expect(page.locator('#text')).toHaveText('Willkommen auf der Seite!')
+
+  await goto('/de/activity', { waitUntil: 'hydration' })
+
+  await page.click('#activity-locale')
+  await expect(page).toHaveURL('/de/change-buchen')
+  await expect(page.locator('#activity-locale-skiing')).toHaveAttribute('href', '/de/change-buchen/aktivitaet-buchen/ski-fahren')
+  await expect(page.locator('#activity-locale-hiking')).toHaveAttribute('href', '/de/change-buchen/hiking')
+
+  await page.click('#activity-locale-hiking')
+  await expect(page).toHaveURL('/de/change-buchen/hiking')
+
+  await page.click('#activity-locale')
+
+  await page.click('#activity-locale-skiing')
+  await expect(page).toHaveURL('/de/change-buchen/aktivitaet-buchen/ski-fahren')
+})

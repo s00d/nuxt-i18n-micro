1a38dd8d43e8c451e467d13a0e75bebea56aabb5
Pavel Kuzmin <Virus191288@gmail.com>
2024-09-01 16:04:04 +0500

    test: add tests for locales path

---
+++ test/fixtures/undefault/nuxt.config.ts
@@ -0,0 +1,25 @@
+import MyModule from '../../../src/module'
+
+export default defineNuxtConfig({
+  modules: [
+    MyModule,
+  ],
+
+  i18n: {
+    locales: [
+      { code: 'en', iso: 'en_EN' },
+      { code: 'de', iso: 'de_DE' },
+      { code: 'ru', iso: 'ru_RU' },
+    ],
+    meta: true,
+    defaultLocale: 'en',
+    translationDir: 'locales',
+    autoDetectLanguage: true,
+    includeDefaultLocaleRoute: true,
+    routesLocaleLinks: {
+      'dir1-slug': 'index',
+    },
+  },
+
+  compatibilityDate: '2024-08-16',
+})

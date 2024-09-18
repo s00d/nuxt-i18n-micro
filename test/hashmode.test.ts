3f36783ed98bda70fcf2d0cd8ddb3f5df1e366d8
Pavel Kuzmin <Virus191288@gmail.com>
2024-09-18 18:34:11 +0500

    test: HashMode

---
+++ test/hashmode.test.ts
@@ -0,0 +1,33 @@
+// Import necessary modules
+import { fileURLToPath } from 'node:url'
+import { expect, test } from '@nuxt/test-utils/playwright'
+
+// Use Nuxt fixture for testing
+test.use({
+  nuxt: {
+    rootDir: fileURLToPath(new URL('./fixtures/hashmode', import.meta.url)),
+  },
+})
+
+test('test language detection and locale handling in hash mode', async ({ page, goto }) => {
+  // Set the 'user-locale' cookie to 'vi' before navigation
+  await page.context().addCookies([{
+    name: 'user-locale',
+    value: 'de',
+    domain: 'localhost',
+    path: '/',
+    expires: -1, // Session cookie
+  }])
+  await goto('/', { waitUntil: 'networkidle' })
+  // Navigate to the URL with hash indicating the locale
+  await goto('/de/#/de', { waitUntil: 'networkidle' })
+
+  // Get the current URL
+  const currentURL = page.url()
+
+  // Verify that the current URL contains the correct hash with the locale
+  expect(currentURL).toContain('/de/#/de')
+
+  // Check that the locale displayed on the page matches the expected value
+  await expect(page.locator('#locale')).toHaveText('de')
+})

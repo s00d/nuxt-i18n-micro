c0b320b1f68308207718659fbe32d47cf7e1609d
Pavel Kuzmin <Virus191288@gmail.com>
2024-11-04 23:51:41 +0500

    fix: isCloudflarePages and custom-fallback-route

--- test/fixtures/hub-i18n/app/pages/index.vue
+++ test/fixtures/hub-i18n/app/pages/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
 const colors = ['#f87171', '#fb923c', '#fbbf24', '#facc15', '#a3e635', '#4ade80', '#34d399', '#2dd4bf', '#22d3ee', '#38bdf8', '#60a5fa', '#818cf8', '#a78bfa', '#c084fc', '#e879f9', '#f472b6', '#fb7185']
 const color = useState('color', () => colors[Math.floor(Math.random() * colors.length)])
 </script>

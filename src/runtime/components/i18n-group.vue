efc0c731da3c88cc5f96947e9b8b5616b96d8602
Pavel Kuzmin <Virus191288@gmail.com>
2024-11-09 17:58:10 +0500

    feat: add i18n-group

---
+++ src/runtime/components/i18n-group.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="['i18n-group', groupClass]">
+    <slot
+      :prefix="prefix"
+      :t="translate"
+    />
+  </div>
+</template>
+
+<script setup>
+import { useI18n } from '../composables/useI18n'
+
+const props = defineProps({
+  prefix: { type: String, required: true },
+  groupClass: { type: String, default: '' },
+})
+
+const { t } = useI18n()
+const translate = (key, params = {}) => t(`${props.prefix}.${key}`, params)
+</script>

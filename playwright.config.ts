965e0263928cb055d76639960065e788b7d3af8e
Pavel Kuzmin <Virus191288@gmail.com>
2024-11-28 15:08:13 +0500

    sync

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './test', // Путь к вашим тестам
+  testMatch: '*.test.ts',
   retries: 3, // Количество повторов тестов при падении
   testIgnore: [
     'test/performance.test.ts', // Игнорируемый файл

274ec1a55823a795ca3f8646bfb4aeb7898c6133
Pavel Kuzmin <Virus191288@gmail.com>
2024-12-03 11:53:39 +0500

    refactor(translation): rename translationHelper to translation-helper

--- src/runtime/translation-server-middleware.ts
+++ src/runtime/translation-server-middleware.ts
@@ -1,7 +1,7 @@
 import type { H3Event } from 'h3'
 import { getQuery, getCookie } from 'h3'
 import type { Translations } from './plugins/01.plugin'
-import { useTranslationHelper } from './translationHelper'
+import { useTranslationHelper } from './translation-helper'
 
 async function fetchTranslations(locale: string): Promise<Translations> {
   try {

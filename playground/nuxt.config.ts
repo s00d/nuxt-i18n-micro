c8b256a88fc7a9474656b64f7c020feb7531caca
Pavel Kuzmin <Virus191288@gmail.com>
2024-12-04 12:55:20 +0500

    fix

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -12,7 +12,7 @@ const routesLocaleLinks: Record<string, string> = {
 export default defineNuxtConfig({
 
   modules: [
-    '../src/module',
+    'nuxt-i18n-micro',
     defineNuxtModule({
       setup(_, nuxt) {
         if (!nuxt.options.dev)

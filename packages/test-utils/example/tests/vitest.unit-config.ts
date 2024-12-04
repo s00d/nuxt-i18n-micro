fff686848e3193eaf89ef9f503fd183430f351ef
Pavel Kuzmin <Virus191288@gmail.com>
2024-12-04 18:55:07 +0500

    fix

--- packages/test-utils/example/tests/vitest.unit-config.ts
+++ packages/test-utils/example/tests/vitest.unit-config.ts
@@ -2,33 +2,8 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
-    environment: 'nuxt',
-    // globals: true,
     watch: false,
-    clearMocks: true,
-    mockReset: true,
-    restoreMocks: true,
     setupFiles: ['./tests/unit-setup.ts'],
     include: ['./**/*.spec.ts'],
-    exclude: [
-      './node_modules',
-      './dist',
-      './automation',
-      './.wrangler',
-      './.nuxt',
-      './coverage',
-      './playwright-report',
-      './tests/e2e',
-      './**/*.e2e.spec.ts',
-      './server',
-    ],
-    environmentOptions: {
-      nuxt: {
-        domEnvironment: 'happy-dom',
-      },
-    },
-    chaiConfig: {
-      truncateThreshold: 0,
-    },
   },
 })

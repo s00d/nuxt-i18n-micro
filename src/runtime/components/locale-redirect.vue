6bebcede28e56daf1470f8d68307aa738db36a04
Pavel Kuzmin <Virus191288@gmail.com>
2025-01-20 11:45:02 +0500

    feat(locale-redirect): implement client-side redirection logic
    Add a `handleRedirect` function to manage locale redirection.
    This function uses `window.history.replaceState` for client-side
    navigations and `navigateTo` for server-side redirects, ensuring
    that users are redirected properly based on the current route.
    This improves the handling of locale changes and avoids unnecessary
    navigations when the current path matches the target path.

--- src/runtime/components/locale-redirect.vue
+++ src/runtime/components/locale-redirect.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup>
-import { useRoute, useI18n, createError, navigateTo } from '#imports'
+import { useRoute, useI18n, createError } from '#imports'
 
 const route = useRoute()
 const { $getLocales, $defaultLocale } = useI18n()
@@ -32,22 +32,38 @@ const currentPageName = generateRouteName(pathSegments)
 const currentLocalePageName = generateLocaleRouteName(pathSegments)
 const globalLocaleRoutes = route.meta.globalLocaleRoutes ?? {}
 
+const handleRedirect = (path) => {
+  if (import.meta.client) {
+    window.history.replaceState(null, '', path) // Заменяем текущую запись в истории
+    location.assign(path) // Перенаправляем на целевую страницу
+  }
+  else {
+    navigateTo(path, { redirectCode: 301, external: true })
+  }
+}
+
 if (globalLocaleRoutes && globalLocaleRoutes[currentPageName]) {
   const localizedRoutes = globalLocaleRoutes[currentPageName]
   if (localizedRoutes && localizedRoutes[defaultLocale]) {
     const localizedPath = localizedRoutes[defaultLocale]
-    navigateTo(localizedPath, { redirectCode: 301, external: true })
+    if (route.fullPath !== localizedPath) {
+      handleRedirect(localizedPath)
+    }
   }
 }
 else if (!locales.includes(firstSegment)) {
   const newPath = `/${defaultLocale}${route.fullPath}`
-  navigateTo(newPath, { redirectCode: 301, external: true })
+  if (route.fullPath !== newPath) {
+    handleRedirect(newPath)
+  }
 }
 else if (locales.includes(firstSegment) && globalLocaleRoutes && globalLocaleRoutes[currentLocalePageName]) {
   const localizedRoutes = globalLocaleRoutes[currentLocalePageName]
   if (localizedRoutes && localizedRoutes[firstSegment]) {
     const localizedPath = `/${firstSegment}${localizedRoutes[firstSegment]}`
-    navigateTo(localizedPath, { redirectCode: 301, external: true })
+    if (route.fullPath !== localizedPath) {
+      handleRedirect(localizedPath)
+    }
   }
 }
 else {

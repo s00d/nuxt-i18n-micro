9bd9e373a4977aa054394f79576cbd38ffb7e6e0
Pavel Kuzmin <Virus191288@gmail.com>
2025-01-22 17:42:50 +0500

    feat(plugins): add i18n registration hook to custom plugin
    Introduce a new plugin in `02.myHook.ts` that registers an i18n hook.
    The plugin logs the locale and registers translations when the `i18n:register`
    event is triggered. This enhances i18n support by allowing dynamic
    translation registration within the Nuxt application.

--- playground/plugins/03.myPlugin.ts
+++ playground/plugins/03.myPlugin.ts
@@ -1,16 +1,12 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  // const { $t, $getLocale } = useI18n() // or const { $t, $getLocale } = useNuxtApp()
-  // const translatedMessage = $t('test_key')
-  // const locale = $getLocale() // error here
-  //
-  // console.log(translatedMessage, locale)
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  nuxtApp.hook('i18n:register', async (register: (translations: unknown, locale?: string) => void, locale: string) => {
-    console.log('i18n:register', locale)
-    register({
-      hook: 'hook value',
-    }, locale)
-  })
+export default defineNuxtPlugin({
+  name: 'i18n:plugin',
+  dependsOn: ['i18n-plugin-loader'],
+  enforce: 'post',
+  setup(_nuxtApp) {
+    // const { $t, $getLocale } = useI18n() // or const { $t, $getLocale } = useNuxtApp()
+    // const translatedMessage = $t('test_key')
+    // const locale = $getLocale() // error here
+    //
+    // console.log(translatedMessage, locale)
+  },
 })

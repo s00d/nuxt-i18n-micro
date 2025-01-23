c8a0d96345aabc2818fe0bdebe3a02821ed37633
Pavel Kuzmin <Virus191288@gmail.com>
2025-01-23 11:49:44 +0500

    feat(playground): add initialization and type check scripts to package.json
    Add `init`, `typecheck`, and `test:types` scripts to `playground/package.json`
    to facilitate project setup and TypeScript type checking.
    
    Refactor plugin to simplify the export statement and update locale button
    keying in `subpage.vue` for improved clarity and functionality.
    
    Include new locale structure in the internationalization configuration.

--- playground/plugins/03.myPlugin.ts
+++ playground/plugins/03.myPlugin.ts
@@ -1,12 +1,7 @@
-export default defineNuxtPlugin({
-  name: 'i18n:plugin',
-  dependsOn: ['i18n-plugin-loader'],
-  enforce: 'post',
-  setup(_nuxtApp) {
-    // const { $t, $getLocale } = useI18n() // or const { $t, $getLocale } = useNuxtApp()
-    // const translatedMessage = $t('test_key')
-    // const locale = $getLocale() // error here
-    //
-    // console.log(translatedMessage, locale)
-  },
+export default defineNuxtPlugin((_nuxtApp) => {
+  // const { $t, $getLocale } = useI18n() // or const { $t, $getLocale } = useNuxtApp()
+  // const translatedMessage = $t('test_key')
+  // const locale = $getLocale() // error here
+  //
+  // console.log(translatedMessage, locale)
 })

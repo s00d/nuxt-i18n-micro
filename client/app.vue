69b12fde047cdb3cfff5ec1ce51f86a6cd14a6f8
Pavel Kuzmin <Virus191288@gmail.com>
2025-01-27 15:00:31 +0500

    feat(app): implement tab navigation for i18n, settings, and config views
    Refactor the main layout of `app.vue` to include a tabbed interface,
    allowing users to switch between the i18n, settings, and config views.
    This enhances the user experience by organizing content and
    improving accessibility. The previous components for locale
    selection and file management were removed, streamlining the
    interface.

--- client/app.vue
+++ client/app.vue
@@ -1,132 +1,46 @@
 <template>
-  <div class="h-screen overflow-auto">
-    <NLoading v-if="isLoading" />
-    <NSplitPane storage-key="tab-i18n-locales">
-      <template #left>
-        <LocalesList
-          :locales="filteredLocales"
-          :selected-locale="selectedLocale"
-          @locale-selected="handleLocaleSelected"
-        />
-      </template>
+  <div
+    class="h-screen overflow-auto"
+  >
+    <!-- Вкладки -->
+    <Tabs
+      v-model="activeTab"
+      :tabs="tabs"
+    />
 
-      <template #right>
-        <div
-          v-if="selectedLocale"
-          h-full
-          of-hidden
-          flex="~ col"
-        >
-          <NSplitPane
-            storage-key="tab-i18n-files"
-            :horizontal="true"
-          >
-            <template #left>
-              <FilesList
-                :files="selectedLocaleFiles"
-                :selected-file="selectedFile"
-                @file-selected="handleFileSelected"
-              />
-            </template>
-            <template #right>
-              <div
-                v-if="selectedFileContent"
-                class="actions"
-              >
-                <NButton @click="exportTranslations">
-                  Export Translations
-                </NButton>
-                <NButton @click="importTranslationsClick">
-                  Import Translations
-                </NButton>
-                <input
-                  v-show="false"
-                  ref="file"
-                  type="file"
-                  @change="importTranslations"
-                >
-              </div>
-              <JsonEditorVue
-                v-if="selectedFileContent"
-                v-model="selectedFileContent"
-                v-bind="$attrs"
-                style="overflow: auto;height: 90%"
-                :main-menu-bar="false"
-                :navigation-bar="false"
-                :status-bar="false"
-                :read-only="false"
-                :indentation="2"
-                :tab-size="2"
-              />
-            </template>
-          </NSplitPane>
-        </div>
-        <NPanelGrids v-else>
-          <NCard
-            px6
-            py4
-          >
-            Select a locale to view locale.<br>Learn more about
-            <NLink
-              href="https://github.com/s00d/nuxt-i18n-micro"
-              n="orange"
-              target="_blank"
-            >
-              i18n
-            </NLink>
-          </NCard>
-        </NPanelGrids>
-      </template>
-    </NSplitPane>
+    <!-- Основной контент -->
+    <I18nView v-if="activeTab === 'i18n'" />
+    <SettingsView v-if="activeTab === 'settings'" />
+    <ConfigView v-if="activeTab === 'config'" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch } from 'vue'
+import { ref, watch } from 'vue'
 import { onDevtoolsClientConnected } from '@nuxt/devtools-kit/iframe-client'
-import JsonEditorVue from 'json-editor-vue'
+import Tabs from './components/Tabs.vue'
+import I18nView from './views/I18nView.vue'
+import SettingsView from './views/SettingsView.vue'
+import ConfigView from './views/ConfigView.vue'
+import { type LocaleData, RPC_NAMESPACE } from './types'
+import { useI18nStore } from './stores/useI18nStore'
 
-export interface TranslationContent {
-  [key: string]: unknown
-}
+const {
+  isLoading,
+  locales,
+  configs,
+  selectedLocale,
+  selectedFile,
+  selectedFileContent,
+} = useI18nStore()
 
-export interface LocaleData {
-  locale: string
-  files: string[]
-  content: Record<string, TranslationContent>
-}
+const activeTab = ref('i18n') // По умолчанию активна вкладка i18n
 
-const RPC_NAMESPACE = 'nuxt-i18n-micro'
-
-const file = ref<HTMLButtonElement | null>(null)
-const isLoading = ref(true)
-const selectedLocale = ref<string>('')
-const selectedFile = ref<string>('')
-const locales = ref<LocaleData[]>([])
-const selectedFileContent = ref<TranslationContent | null>(null)
-
-const selectedLocaleFiles = computed<string[]>(() => {
-  const locale = locales.value.find(l => l.locale === selectedLocale.value)
-  return locale ? locale.files : []
-})
-
-const filteredLocales = computed<LocaleData[]>(() => {
-  // фильтрация теперь обрабатывается в компоненте LocalesList
-  return locales.value
-})
-
-const handleLocaleSelected = (locale: string) => {
-  selectedLocale.value = locale
-}
-
-const handleFileSelected = (file: string) => {
-  selectedFile.value = file
-}
-
-watch([selectedLocale, selectedFile], () => {
-  const locale = locales.value.find(l => l.locale === selectedLocale.value)
-  selectedFileContent.value = locale ? locale.content[selectedFile.value] : null
-})
+const tabs = [
+  { label: 'i18n', value: 'i18n' },
+  { label: 'Settings', value: 'settings' },
+  { label: 'Server Info', value: 'config' },
+]
 
 onDevtoolsClientConnected(async (client) => {
   const rpc = client.devtools.extendClientRpc(RPC_NAMESPACE, {
@@ -139,14 +53,14 @@ onDevtoolsClientConnected(async (client) => {
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
-  const initialLocales = await rpc.getLocalesAndTranslations()
-  locales.value = initialLocales
+  locales.value = await rpc.getLocalesAndTranslations()
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  configs.value = await rpc.getConfigs()
   isLoading.value = false
 
   watch(selectedFileContent, (newContent, oldValue) => {
-    if (!oldValue) {
-      return
-    }
+    if (!oldValue) return
     if (selectedLocale.value && selectedFile.value && newContent) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-expect-error
@@ -154,36 +68,4 @@ onDevtoolsClientConnected(async (client) => {
     }
   }, { deep: true })
 })
-
-const exportTranslations = async () => {
-  const blob = new Blob([JSON.stringify(selectedFileContent.value, null, 2)], { type: 'application/json' })
-  const url = URL.createObjectURL(blob)
-  const link = document.createElement('a')
-  link.href = url
-  link.download = 'translations.json'
-  link.click()
-  URL.revokeObjectURL(url)
-}
-
-const importTranslationsClick = () => {
-  file.value?.click()
-}
-
-const importTranslations = (event: Event) => {
-  const input = event.target as HTMLInputElement
-  if (input.files && input.files.length > 0) {
-    const file = input.files[0]
-    const reader = new FileReader()
-    reader.onload = (e) => {
-      try {
-        const content = JSON.parse(e.target?.result as string)
-        selectedFileContent.value = content
-      }
-      catch {
-        console.error('Invalid JSON file')
-      }
-    }
-    reader.readAsText(file)
-  }
-}
 </script>

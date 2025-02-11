3d69b45d3458e101d1e343bdb99969c1b04ea1b0
Jowo <ignesia@gmail.com>
2025-02-11 21:26:47 +0100

    fix(i18n): fallbackLocale reimplementation

--- src/runtime/server/routes/get.ts
+++ src/runtime/server/routes/get.ts
@@ -34,41 +34,49 @@ function deepMerge(target: Translations, source: Translations): Translations {
   return target
 }
 
-async function loadTranslations(rootDirs: string[], translationDir: string, locale: string, page: string): Promise<Translations> {
+async function loadTranslations(rootDirs: string[], translationDir: string, locale: string, page: string, fallbackLocale?: string): Promise<Translations> {
   const getTranslationPath = (locale: string, page: string) =>
     page === 'general' ? `${locale}` : `pages/${page}/${locale}`
 
   let translations: Translations = {}
 
-  const createPaths = (locale: string, ext: string) =>
-    rootDirs.map(dir => ({
-      translationPath: resolve(dir, translationDir!, `${getTranslationPath(locale, page)}${ext}`),
-      name: `_locales/${getTranslationPath(locale, page)}${ext}`,
-    }))
+  const loadLocale = async (currentLocale: string) => {
+    const extensions = ['.json', '.yaml', '.yml']
+    const paths = extensions.flatMap(ext => createPaths(currentLocale, ext))
 
-  const extensions = ['.json', '.yaml', '.yml']
-  const paths = extensions.flatMap(ext => createPaths(locale, ext))
+    for (const { translationPath } of paths) {
+      try {
+        const content = await readFile(translationPath, 'utf-8')
+        let fileContent: Translations = {}
 
-  for (const { translationPath } of paths) {
-    try {
-      const content = await readFile(translationPath, 'utf-8')
-      let fileContent: Translations = {}
+        if (translationPath.endsWith('.json')) {
+          fileContent = JSON.parse(content) as Translations
+        }
+        else if (translationPath.endsWith('.yaml') || translationPath.endsWith('.yml')) {
+          fileContent = await loadYaml(translationPath) as Translations
+        }
 
-      if (translationPath.endsWith('.json')) {
-        fileContent = JSON.parse(content) as Translations
+        translations = deepMerge(translations, fileContent)
       }
-      else if (translationPath.endsWith('.yaml') || translationPath.endsWith('.yml')) {
-        fileContent = await loadYaml(translationPath) as Translations
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      catch (e) {
+        // We ignore the error here, as it just means that the file doesn't exist which is fine
+        // console.error('[nuxt-i18n-micro] load locale error', e)
       }
-
-      translations = deepMerge(translations, fileContent)
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    catch (e) {
-      // We ignore the error here, as it just means that the file doesn't exist which is fine
-      // console.error('[nuxt-i18n-micro] load locale error', e)
     }
   }
+
+  const createPaths = (currentLocale: string, ext: string) =>
+    rootDirs.map(dir => ({
+      translationPath: resolve(dir, translationDir!, `${getTranslationPath(currentLocale, page)}${ext}`),
+      name: `_locales/${getTranslationPath(currentLocale, page)}${ext}`,
+    }))
+
+  if (fallbackLocale && fallbackLocale !== locale) {
+    await loadLocale(fallbackLocale)
+  }
+
+  await loadLocale(locale)
 
   return translations
 }
@@ -76,7 +84,7 @@ async function loadTranslations(rootDirs: string[], translationDir: string, loca
 export default defineEventHandler(async (event) => {
   const { page, locale } = event.context.params as { page: string, locale: string }
   const config = useRuntimeConfig()
-  const { rootDirs, debug, translationDir, customRegexMatcher } = config.i18nConfig as ModulePrivateOptionsExtend
+  const { rootDirs, debug, translationDir, fallbackLocale, customRegexMatcher } = config.i18nConfig as ModulePrivateOptionsExtend
   const { locales } = config.public.i18nConfig as unknown as ModuleOptionsExtend
 
   if (customRegexMatcher && locales && !locales.map(l => l.code).includes(locale)) {
@@ -103,7 +111,7 @@ export default defineEventHandler(async (event) => {
     translations = typeof rawContent === 'string' ? JSON.parse(rawContent) : rawContent
   }
   else {
-    translations = await loadTranslations(rootDirs, translationDir!, locale, page)
+    translations = await loadTranslations(rootDirs, translationDir, locale, page, fallbackLocale)
     await serverStorage.setItem(cacheName, translations)
   }
 

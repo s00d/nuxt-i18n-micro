4894ae7e07778862cbcc8046aaf8e6b0526527c0
Pavel Kuzmin <Virus191288@gmail.com>
2025-05-05 00:31:04 +0500

    test(seo): add SEO tests for canonical URLs and locale attributes

---
+++ test/fixtures/seo/pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>About Page</div>
+</template>
+
+<script setup>
+import { useNuxtApp } from '#imports'
+
+const { $defineI18nRoute } = useNuxtApp()
+
+$defineI18nRoute({
+  localeRoutes: {
+    en: '/about',
+    de: '/a-propos',
+  },
+})
+</script>
---
+++ test/fixtures/seo/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p id="content">
+      {{ $t('key0') }}
+    </p>
+
+    <i18n-link
+      id="contact"
+      :to="{ name: 'contact' }"
+    >
+      Contact
+    </i18n-link>
+
+    <i18n-link
+      id="about"
+      :to="{ name: 'about' }"
+    >
+      About
+    </i18n-link>
+
+    <div id="locale-switcher">
+      <i18n-switcher />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useNuxtApp } from '#imports'
+
+const { $t } = useNuxtApp()
+</script>

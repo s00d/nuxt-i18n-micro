f238ce888cd9f55128bc67c7ab76b1bcd5c1af41
Pavel Kuzmin <Virus191288@gmail.com>
2025-10-10 12:54:59 +0500

    feat(meta): enhance meta generation with route-based checks

--- src/runtime/plugins/02.meta.ts
+++ src/runtime/plugins/02.meta.ts
@@ -1,14 +1,27 @@
 import type { ModuleOptionsExtend } from 'nuxt-i18n-micro-types'
 import { useLocaleHead } from '../composables/useLocaleHead'
-import { useRequestURL, useHead, defineNuxtPlugin, useRuntimeConfig } from '#imports'
+import { useRequestURL, useHead, defineNuxtPlugin, useRuntimeConfig, useRoute } from '#imports'
+import { isMetaDisabledForRoute } from '../utils/route-utils'
 
 const host = process.env.HOST ?? 'localhost'
 const port = process.env.PORT ?? 'host'
 
-export default defineNuxtPlugin((_nuxtApp) => {
+export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
+  const route = useRoute()
 
   const i18nConfig: ModuleOptionsExtend = config.public.i18nConfig as unknown as ModuleOptionsExtend
+
+  // Get current locale
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const currentLocale = nuxtApp.$getLocale?.()
+
+  // Check if meta is disabled for this route
+  if (isMetaDisabledForRoute(route, i18nConfig.routeDisableMeta, currentLocale)) {
+    // Don't generate any meta tags if disabled for this route
+    return
+  }
 
   const schema = port === '443' ? 'https' : 'http'
   const defaultUrl = port === '80' || port === '443' ? `${schema}://${host}` : `${schema}://${host}:${port}`

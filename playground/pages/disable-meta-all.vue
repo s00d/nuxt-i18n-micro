13ff53166352518b33c842afdc73e2ec55b38f15
Pavel Kuzmin <Virus191288@gmail.com>
2025-10-10 12:55:40 +0500

    feat(pages): add pages to control i18n meta tag visibility
    Create `disable-meta-locale.vue` to disable meta tags for the English
    locale while keeping them for others. Create `disable-meta-all.vue`
    to completely disable meta tags for all locales. These pages help
    illustrate meta tag behavior in an i18

---
+++ playground/pages/disable-meta-all.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Disable Meta All</h1>
+    <p>This page has meta tags completely disabled for all locales</p>
+    <p>Check the page source - no i18n meta tags should be present</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $defineI18nRoute } = useNuxtApp()
+
+$defineI18nRoute({
+  locales: ['en', 'fr', 'de'],
+  disableMeta: true,
+})
+</script>
---
+++ playground/pages/disable-meta-locale.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Disable Meta Locale</h1>
+    <p>This page has meta tags disabled only for English locale</p>
+    <p>Check the page source:</p>
+    <ul>
+      <li>/disable-meta-locale - no i18n meta tags</li>
+      <li>/fr/disable-meta-locale - i18n meta tags present</li>
+      <li>/de/disable-meta-locale - i18n meta tags present</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $defineI18nRoute } = useNuxtApp()
+
+$defineI18nRoute({
+  locales: ['de'],
+  disableMeta: ['en'],
+})
+</script>

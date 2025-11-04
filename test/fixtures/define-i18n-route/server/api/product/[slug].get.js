c71a0a066b2095b288e9a553c6a29dccd73d7e41
Pavel Kuzmin <Virus191288@gmail.com>
2025-11-04 23:24:40 +0500

    fix(api): correct object syntax and import for event handler

--- test/fixtures/define-i18n-route/server/api/product/[slug].get.js
+++ test/fixtures/define-i18n-route/server/api/product/[slug].get.js
@@ -1,32 +1,33 @@
+import { defineEventHandler, getRouterParam } from 'h3'
+
 const products = {
   'coffee-filter-en': {
     title: 'Coffee Filter',
     price: '$18.00',
     urlEn: 'coffee-filter-en',
-    urlEs: 'filtro-cafe-es'
+    urlEs: 'filtro-cafe-es',
   },
   'filtro-cafe-es': {
     title: 'Filtro de Café',
     price: '18,00 €',
     urlEn: 'coffee-filter-en',
-    urlEs: 'filtro-cafe-es'
+    urlEs: 'filtro-cafe-es',
   },
   'solar-power-bank': {
     title: 'Solar Power Bank',
     price: '$119.95',
     urlEn: 'solar-power-bank',
-    urlEs: 'banco-energia-solar'
+    urlEs: 'banco-energia-solar',
   },
   'banco-energia-solar': {
     title: 'Banco de Energía Solar',
     price: '119,95 €',
     urlEn: 'solar-power-bank',
-    urlEs: 'banco-energia-solar'
-  }
+    urlEs: 'banco-energia-solar',
+  },
 }
 
-export default eventHandler((event) => {
+export default defineEventHandler((event) => {
   const slug = getRouterParam(event, 'slug')
   return products[slug] || null
 })
-

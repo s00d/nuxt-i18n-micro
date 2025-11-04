a2169ef848372904f26a0a246d457a0fc5d138a8
Pavel Kuzmin <Virus191288@gmail.com>
2025-11-04 23:24:40 +0500

    fix(api): correct object syntax and import for event handler

--- test/fixtures/define-i18n-route/server/api/product/index.get.js
+++ test/fixtures/define-i18n-route/server/api/product/index.get.js
@@ -1,33 +1,34 @@
-export default eventHandler(async (event) => {
+import { defineEventHandler } from 'h3'
+
+export default defineEventHandler(async () => {
   return {
     en: [
       {
         id: 'prod001',
         title: 'Coffee Filter',
         price: '$18.00',
-        url: 'coffee-filter-en'
+        url: 'coffee-filter-en',
       },
       {
         id: 'prod002',
         title: 'Solar Power Bank',
         price: '$119.95',
-        url: 'solar-power-bank'
-      }
+        url: 'solar-power-bank',
+      },
     ],
     es: [
       {
         id: 'prod001',
         title: 'Filtro de Café',
         price: '18,00 €',
-        url: 'filtro-cafe-es'
+        url: 'filtro-cafe-es',
       },
       {
         id: 'prod002',
         title: 'Banco de Energía Solar',
         price: '119,95 €',
-        url: 'banco-energia-solar'
-      }
-    ]
+        url: 'banco-energia-solar',
+      },
+    ],
   }
 })
-

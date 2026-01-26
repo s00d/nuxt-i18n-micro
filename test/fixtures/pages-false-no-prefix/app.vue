809b114504292f79c4e77e18215f4f0236ab45f0
Pavel Kuzmin <Virus191288@gmail.com>
2026-01-26 12:57:00 +0500

    test: add reactivity tests for i18n locale switching and routing

---
+++ test/fixtures/pages-false-no-prefix/app.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useNuxtApp } from '#imports'
+
+const { $getLocale, $t, $switchLocale, $mergeGlobalTranslations } = useNuxtApp()
+
+// For no_prefix with pages: false, we need to manually reload translations
+async function switchToZh() {
+  // Load Chinese translations
+  const data = await $fetch('/locales/zh.json')
+  $mergeGlobalTranslations(data)
+  $switchLocale('zh')
+}
+
+async function switchToEn() {
+  // Load English translations
+  const data = await $fetch('/locales/en.json')
+  $mergeGlobalTranslations(data)
+  $switchLocale('en')
+}
+</script>
+
+<template>
+  <div>
+    <p id="locale">
+      {{ $getLocale() }}
+    </p>
+    <p id="greeting">
+      {{ $t('hello') }}
+    </p>
+    <button
+      id="switch-zh"
+      @click="switchToZh"
+    >
+      Switch to Chinese
+    </button>
+    <button
+      id="switch-en"
+      @click="switchToEn"
+    >
+      Switch to English
+    </button>
+  </div>
+</template>

4fd130d74e914eaea64ff09082242305cd5d0911
Pavel Kuzmin <Virus191288@gmail.com>
2026-01-26 12:55:54 +0500

    test(auto-detect-no-prefix): add tests for autoDetectLanguage functionality

---
+++ test/fixtures/auto-detect-no-prefix/nuxt.config.ts
@@ -0,0 +1,25 @@
+import MyModule from '../../../src/module'
+
+export default defineNuxtConfig({
+  modules: [
+    MyModule,
+  ],
+  devtools: { enabled: false },
+  compatibilityDate: '2024-08-16',
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  i18n: {
+    locales: [
+      { code: 'en', iso: 'en_EN' },
+      { code: 'de', iso: 'de_DE' },
+      { code: 'ru', iso: 'ru_RU' },
+    ],
+    localeCookie: 'user-locale',
+    autoDetectLanguage: true,
+    strategy: 'no_prefix',
+    defaultLocale: 'en',
+    translationDir: 'locales',
+    // disablePageLocales: false is default
+  },
+})
---
+++ test/fixtures/auto-detect-no-prefix/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useNuxtApp } from '#imports'
+
+const { $getLocale, $t } = useNuxtApp()
+</script>
+
+<template>
+  <div>
+    <p id="locale">
+      {{ $getLocale() }}
+    </p>
+    <p id="greeting">
+      {{ $t('greeting') }}
+    </p>
+  </div>
+</template>

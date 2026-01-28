e0da906f9bb2e48fc35eebeb8b3f25ddc2af91bc
Pavel Kuzmin <Virus191288@gmail.com>
2026-01-28 20:24:27 +0500

    feat(route-generator): add utility functions for path and route handling

---
+++ packages/route-generator/src/utils/index.ts
@@ -0,0 +1,18 @@
+export {
+  normalizePath,
+  normalizeRouteKey,
+  removeLeadingSlash,
+  joinPath,
+  buildFullPath,
+  buildFullPathNoPrefix,
+} from './path'
+
+export {
+  cloneArray,
+  isPageRedirectOnly,
+  buildRouteName,
+  buildRouteNameFromRoute,
+  shouldAddLocalePrefix,
+  isLocaleDefault,
+  isInternalPath,
+} from './common'

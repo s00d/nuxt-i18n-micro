9327b5e1804042539f9c2d0a4624bf54b791c54e
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-03 18:44:37 +0500

    feat(playwright): add CI-specific configurations for tracing, video, and screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,16 @@ export default defineConfig({
   testMatch: '*.spec.ts',
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : 3,
+  use: {
+    // В CI сохранять трейс при ошибке, локально — выключить
+    trace: process.env.CI ? 'retain-on-failure' : 'off',
+
+    // В CI сохранять видео при ошибке, локально — выключить
+    video: process.env.CI ? 'retain-on-failure' : 'off',
+
+    // Скриншоты только при ошибке
+    screenshot: 'only-on-failure',
+  },
   testIgnore: [],
   projects: [
     {

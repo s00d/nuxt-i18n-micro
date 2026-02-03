72cf7775efd9c2eb137a4225c6bee51a7fa37e69
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-03 18:47:55 +0500

    refactor(translation-server-middleware): replace runtime config access method

--- src/runtime/translation-server-middleware.ts
+++ src/runtime/translation-server-middleware.ts
@@ -1,19 +1,18 @@
 import type { H3Event } from 'h3'
 import { interpolate, useTranslationHelper } from '@i18n-micro/core'
 import type { TranslationCache } from '@i18n-micro/core'
-import type { ModuleOptionsExtend, ModulePrivateOptionsExtend, Params, Translations } from '@i18n-micro/types'
+import type { ModuleOptionsExtend, Params, Translations } from '@i18n-micro/types'
 import { detectCurrentLocale } from './utils/locale-detector'
-import { useRuntimeConfig } from '#imports'
+import { getI18nConfig } from '#i18n-internal/strategy'
 
 // Constant for the request context key to avoid typos
 const I18N_CONTEXT_KEY = '__i18n_cache__'
 
 async function fetchTranslations(locale: string): Promise<Translations> {
   try {
-    const config = useRuntimeConfig() as { i18nConfig?: ModulePrivateOptionsExtend }
-    const i18nConfig = config.i18nConfig
-    const apiBaseUrl = i18nConfig?.apiBaseUrl ?? '_locales'
-    const apiBaseServerHost = i18nConfig?.apiBaseServerHost ?? undefined
+    const i18nConfig = getI18nConfig() as ModuleOptionsExtend
+    const apiBaseUrl = i18nConfig.apiBaseUrl ?? '_locales'
+    const apiBaseServerHost = i18nConfig.apiBaseServerHost ?? undefined
     // IMPORTANT: On the server, use a full URL or a configured baseURL for $fetch.
     // If the endpoint is on the same server, Nuxt/Nitro will handle it internally.
     let url = `/${apiBaseUrl}/general/${locale}/data.json`
@@ -52,8 +51,7 @@ export const useTranslationServerMiddleware = async (event: H3Event, defaultLoca
   // The helper will now operate on data unique to this request
   const { getTranslation, loadTranslations, hasGeneralTranslation } = useTranslationHelper(requestScopedCache)
 
-  const config = useRuntimeConfig(event).public.i18nConfig as unknown as ModuleOptionsExtend // Changed to public.i18nConfig, as locales are there
-  const { locales, fallbackLocale, defaultLocale: configDefaultLocale } = config
+  const { locales, fallbackLocale, defaultLocale: configDefaultLocale } = getI18nConfig() as ModuleOptionsExtend
 
   // Pass locales for URL path check
   const locale = currentLocale || detectCurrentLocale(event, {

d10a2ffafad771cb733b285655f98dc4ef7f9af2
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-05 15:04:45 +0500

    feat(server): update translation middleware to use new storage structure

--- src/runtime/server/utils/translation-server-middleware.ts
+++ src/runtime/server/utils/translation-server-middleware.ts
@@ -1,28 +1,22 @@
 import type { H3Event } from 'h3'
 import { interpolate, useTranslationHelper } from '@i18n-micro/core'
-import type { TranslationCache } from '@i18n-micro/core'
+import type { TranslationStorage } from '@i18n-micro/core'
 import type { ModuleOptionsExtend, Params, Translations } from '@i18n-micro/types'
 import { detectCurrentLocale } from './locale-detector'
 import { getI18nConfig } from '#i18n-internal/strategy'
-import { loadLayerTranslations } from './load-from-storage'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - #imports are available in Nitro
-import { useStorage } from '#imports'
+import { loadTranslationsFromServer } from './server-loader'
 
-const I18N_CONTEXT_KEY = '__i18n_cache__'
+const I18N_CONTEXT_KEY = '__i18n_storage__'
 
 export const useTranslationServerMiddleware = async (event: H3Event, defaultLocale?: string, currentLocale?: string) => {
   if (!event.context[I18N_CONTEXT_KEY]) {
     event.context[I18N_CONTEXT_KEY] = {
-      generalLocaleCache: {},
-      routeLocaleCache: {},
-      dynamicTranslationsCaches: [],
-      serverTranslationCache: {},
-    } as TranslationCache
+      translations: new Map<string, Translations>(),
+    } as TranslationStorage
   }
 
-  const requestScopedCache: TranslationCache = event.context[I18N_CONTEXT_KEY]
-  const { getTranslation, loadTranslations, hasGeneralTranslation } = useTranslationHelper(requestScopedCache)
+  const storage: TranslationStorage = event.context[I18N_CONTEXT_KEY]
+  const { getTranslation, loadTranslations, hasGeneralTranslation } = useTranslationHelper(storage)
   const { locales, fallbackLocale, defaultLocale: configDefaultLocale } = getI18nConfig() as ModuleOptionsExtend
 
   const locale = currentLocale || (event.context.i18n?.locale) || detectCurrentLocale(event, {
@@ -32,15 +26,15 @@ export const useTranslationServerMiddleware = async (event: H3Event, defaultLoca
   }, defaultLocale)
 
   if (!hasGeneralTranslation(locale)) {
-    let translations: Translations = {}
+    let translations: Translations
     if (event.context.i18n?.translations && Object.keys(event.context.i18n.translations).length > 0) {
       translations = event.context.i18n.translations
     }
     else {
-      const storage = useStorage()
-      translations = await loadLayerTranslations(storage, locale)
+      const { data } = await loadTranslationsFromServer(locale)
+      translations = data
     }
-    await loadTranslations(locale, translations)
+    loadTranslations(locale, translations)
   }
 
   function t(key: string, params?: Params, defaultValue?: string): string {

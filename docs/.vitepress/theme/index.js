31c9aa1224610564ff366cfcff1c8253c1f3547d
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-06 19:20:37 +0500

    feat(docs): add Chart.js integration and performance test results link
    Integrate `Chart.js` into the documentation configuration to enable
    chart rendering capabilities. Also, add a new link to the
    'Performance Test Results' section for better navigation and user
    access to performance metrics.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,4 +4,11 @@ import './custom.css'
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
+  async enhanceApp({ app }) {
+    // Only load on client (SSR-safe)
+    if (typeof window !== 'undefined') {
+      const { Chart, registerables } = await import('chart.js')
+      Chart.register(...registerables)
+    }
+  },
 }

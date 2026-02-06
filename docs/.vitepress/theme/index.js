b092df760d922ce3e42e57b31954e8627607649e
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-06 20:35:51 +0500

    refactor(docs): simplify enhanceApp function in theme index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,7 +4,7 @@ import './custom.css'
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
-  async enhanceApp({ app }) {
+  async enhanceApp() {
     // Only load on client (SSR-safe)
     if (typeof window !== 'undefined') {
       const { Chart, registerables } = await import('chart.js')

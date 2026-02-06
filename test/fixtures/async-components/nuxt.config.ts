d8eb95d5ec781a46e7c436d5f44c6ca2e249a37d
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-06 20:37:41 +0500

    fix(test/fixtures): update nuxt.config.ts to handle SSR type errors

--- test/fixtures/async-components/nuxt.config.ts
+++ test/fixtures/async-components/nuxt.config.ts
@@ -6,7 +6,9 @@ export default defineNuxtConfig({
   ],
   devtools: { enabled: false },
   routeRules: {
+    // @ts-expect-error ssr is valid in runtime but types are not updated
     '/async-components-test': { ssr: false },
+    // @ts-expect-error ssr is valid in runtime but types are not updated
     '/async-components-test-2': { ssr: false },
   },
   compatibilityDate: '2024-08-16',

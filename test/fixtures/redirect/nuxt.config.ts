0873b6cb540162954f91c2dabb92c1254eaea430
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-06 18:33:42 +0500

    test(redirect): remove unused i18n configuration in redirect tests

--- test/fixtures/redirect/nuxt.config.ts
+++ test/fixtures/redirect/nuxt.config.ts
@@ -19,6 +19,7 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
     translationDir: 'locales',
     autoDetectLanguage: true,
+    autoDetectPath: '*',
     routesLocaleLinks: {
       'dir1-slug': 'index',
     },

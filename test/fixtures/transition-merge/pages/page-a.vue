041de9c5e1a296a131f1eafa7f0c108b56ffb928
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-13 17:56:29 +0500

    test(transition-merge): add tests for deep merging of overlapping nested keys

--- test/fixtures/transition-merge/pages/page-a.vue
+++ test/fixtures/transition-merge/pages/page-a.vue
@@ -3,6 +3,7 @@
     <h1 id="page-a-title">{{ $t('pageA.title') }}</h1>
     <p id="page-a-body">{{ $t('pageA.body') }}</p>
     <p id="page-a-shared">{{ $t('shared') }}</p>
+    <p id="page-a-common">{{ $t('common.fromA') }}</p>
     <i18n-link id="go-to-b" :to="{ name: 'page-b' }">Go to B</i18n-link>
   </div>
 </template>

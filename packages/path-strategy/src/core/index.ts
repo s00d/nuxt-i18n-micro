62a27453ae8beeca90be176f98bb26fdc03c5d08
Pavel Kuzmin <Virus191288@gmail.com>
2026-02-16 18:22:49 +0500

    refactor(path-strategy): remove unused route building and normalization code

--- packages/path-strategy/src/core/index.ts
+++
@@ -1,4 +0,0 @@
-export { createLocalizedRouteObject } from './builder'
-export { getLocaleFromPath, getPathWithoutLocale, type PathWithoutLocaleResult } from './normalizer'
-export { RouteResolver } from './resolver'
-export * from './types'
